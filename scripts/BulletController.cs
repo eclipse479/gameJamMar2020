3895c47dda1af94616da4c163f09e353340d1d5c
Luke Lazzaro <zodiac44@hotmail.com>
2020-03-16 14:27:17 +1100

    yeet

---
+++ scripts/BulletController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public Vector3 startScale;
+    public Vector3 startDirection;
+    public float speed = 200f;
+    public float lifetime = 5f;
+
+    private Rigidbody rb;
+    private float lifeTimer = 0f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.localScale = startScale;
+    }
+
+    void Start()
+    {
+        rb.AddForce(startDirection * speed, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        lifeTimer += Time.deltaTime;
+        if (lifeTimer > lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ scripts/ShootBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class ShootBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    private Vector3 shootDirection;
+    private float shootRate = 0.5f;
+    private float shootTimer;
+
+    void Awake()
+    {
+        shootTimer = shootRate;
+        shootDirection = new Vector3(1f, 0f, 0f);
+    }
+
+    void Update()
+    {
+        float shootX = XCI.GetAxisRaw(XboxAxis.RightStickX, XboxController.All);
+        float shootY = XCI.GetAxisRaw(XboxAxis.RightStickY, XboxController.All);
+
+        if (!(Mathf.Approximately(shootX, 0.0f) && Mathf.Approximately(shootY, 0.0f)))
+        {
+            shootDirection = new Vector3(shootX, shootDirection.y, shootY);
+            shootDirection.Normalize();
+        }
+
+        shootTimer += Time.deltaTime;
+        if (shootTimer > shootRate)
+        {
+            if (!Mathf.Approximately(XCI.GetAxisRaw(XboxAxis.RightTrigger, XboxController.All), 0.0f))
+            {
+                StartCoroutine(ShootThree());
+                shootTimer = 0f;
+            }
+        }
+    }
+
+    IEnumerator ShootThree()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            BulletController bulletSpawn = Instantiate(bulletPrefab, transform.position, Quaternion.identity).GetComponent<BulletController>();
+            bulletSpawn.startDirection = shootDirection;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
