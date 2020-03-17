742e6ec62ee52e471f98faa780e00eb957c3160d
gf <j>
2020-03-17 11:11:41 +1100

    finished basic powerUps

--- scripts/powerUp.cs
+++ scripts/powerUp.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class powerUp : MonoBehaviour
 {
-    enum powerUpType
+    public enum powerUpType
     {
         bouncy,
-        explocive,
+        explosive,
         fast,
-        spread
+        spread,
     };
+
+    public powerUpType currentPower;
+    private int rand;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < 100; i++)
+        {
+        rand = Random.Range(0, 4);
+            Debug.Log(rand);
+        }
+        switch (rand)
+        {
+            case 0:
+                {
+                    currentPower = powerUpType.bouncy;
+                    break;
+                }
+            case 1:
+                {
+                    currentPower = powerUpType.explosive;
+                    break;
+                }
+            case 2:
+                {
+                    currentPower = powerUpType.fast;
+                    break;
+                }
+            case 3:
+                {
+                    currentPower = powerUpType.spread;
+                    break;
+                }
+        }
+
     }
 
     // Update is called once per frame
