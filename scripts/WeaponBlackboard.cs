75eae03356eec3a5e6f985d4ce7d2174437cc22a
Luke Lazzaro <zodiac44@hotmail.com>
2020-03-17 16:33:39 +1100

    Add weapon blackboard

---
+++ scripts/WeaponBlackboard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponBlackboard : MonoBehaviour
+{
+    public static WeaponBlackboard instance;
+
+    public Image gunType;
+    public Sprite normalGun;
+    public Sprite bounceGun;
+    public Sprite explosiveGun;
+    public Sprite fastGun;
+    public Sprite spreadGun;
+    public Sprite burstGun;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+}
